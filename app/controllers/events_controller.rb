70018bbbd35e4ec66fadf4e962a10284ba44ce5d
Jonathan Chang <jonoschang7@gmail.com>
2014-05-01 14:14:56 -0400

    Work on create action for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -11,6 +11,12 @@ class EventsController < ApplicationController
 	end
 
 	def create
+		@event = Event.new(events_params)
+		if @event.save
+			redirect_to events_path
+		else
+			render new
+		end
 	end
 
 	def edit
